cf96aee6da3120af2285aa3c8df31e25848b6791
Deepanshu <dipanshubhanot@gmail.com>
2020-10-03 18:47:47 +1000

    Initial Setup of Dashboard with basic marking in maps

---
+++ src/layout/ContentFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <nav>
+        <ul class="footer-menu">
+          <li>
+            <router-link :to="{path:'/admin'}">Dashboard</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        &copy; Coded with
+        <i class="fa fa-heart heart"></i> by FireBusters. Template by
+        <a href="https://binarcode.com" target="_blank" rel="noopener noreferrer">BinarCode</a>.
+        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank" rel="noopener noreferrer">Creative Tim</a>.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+  export default {}
+
+</script>
+<style>
+
+</style>
